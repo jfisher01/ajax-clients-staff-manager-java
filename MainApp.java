214c8962145049b5d39a082f160a7aa7730c2da1
Johnfisher Uchem <103228361+jfisher01@users.noreply.github.com>
2023-12-24 18:34:53 -0600

    added the files

---
+++ MainApp.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ajax;
+
+
+/**
+ * Main class that contains the driving main method for this application.    
+ * Default Main method for for this application.    
+ * @author Johnfisher Uchem
+ * @version 1.0 / 12/24/2023 
+ * @since Ajax Clients Employees Manager 1.0 
+ * 
+ */
+
+public class MainApp 
+{
+/**
+ * Driver method for this application.   
+ * @param args the args parameter in the main method. 
+ */
+public static void main(String[] args) 
+{
+
+		//LAUNCH APP
+	new ClientInterface().ajaxclientsEmployeesManager();
+	
+}
+
+}
